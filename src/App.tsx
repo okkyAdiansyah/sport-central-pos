9cc1df158b68eb28f565b80ab8971687e75ee4f1
okkyAdiansyah <okkyadiansyah.0610@gmail.com>
2025-06-09 15:06:40 +0700

    Init:Add Shadcn and React-Router-Dom

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+
+function App() {
+
+  return (
+    <>
+      <div id='wrapper' className='flex'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
